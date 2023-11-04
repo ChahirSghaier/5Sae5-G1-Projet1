4d61a0e0a861d898d809807930f77287d249c0d1
Nour Bououn <100091833+nounou5@users.noreply.github.com>
2023-11-04 14:08:53 +0100

    test

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -3,7 +3,6 @@ package tn.esprit.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GestionStationSkiApplicationTests {
 
 	@Test
