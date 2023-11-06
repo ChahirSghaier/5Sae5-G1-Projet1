2e5030be643751f7dc8e626292c97ac3c404d3de
ChahirSghaier <chahir.sghaier@esprit.tn>
2023-11-06 23:45:43 +0100

    Adding Unit Tests for CRUD Course

--- src/test/java/tn/esprit/spring/CourseServiceTest.java
+++ src/test/java/tn/esprit/spring/CourseServiceTest.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.util.AssertionErrors.*;
 
 @AllArgsConstructor
